e588fcab39e66d217270a26897679c3d493202ff
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2021-05-23 17:16:22 +0530

    Basic artwork setup

--- src/components/artwork/artwork.ts
+++ src/components/artwork/artwork.ts
@@ -1,3 +1,33 @@
-class Artwork {}
+class Artwork {
+  canvas: HTMLCanvasElement;
+
+  ctx: CanvasRenderingContext2D;
+
+  height: number;
+
+  width: number;
+
+  constructor(canvas: HTMLCanvasElement) {
+    this.canvas = canvas;
+    this.height = canvas.height;
+    this.width = canvas.width;
+  }
+
+  cleanUp(): void {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+  }
+
+  paintBackground(): void {
+    this.ctx.save();
+    this.ctx.fillStyle = 'black';
+    this.ctx.fillRect(0, 0, this.width, this.height);
+    this.ctx.restore();
+  }
+
+  render(timestamp: number): void {
+    this.cleanUp();
+    this.paintBackground();
+  }
+}
 
 export default Artwork;
